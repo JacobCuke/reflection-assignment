9c821b914acd4ccc1c302198ccc085f20b05c37f
jacob.cuke <jacob.cuke@cs.ucalgary.ca>
2020-10-06 14:24:51 -0600

    Print Class name and SuperClass name recursively

--- reflection-assignment/src/Inspector.java
+++ reflection-assignment/src/Inspector.java
@@ -10,6 +10,8 @@
 import java.lang.reflect.*;
 
 public class Inspector {
+	
+	private int depth = 0;
 
     public void inspect(Object obj, boolean recursive) {
         Class c = obj.getClass();
@@ -18,6 +20,25 @@ public class Inspector {
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
     	
+    	this.depth = depth;
+    	
+    	println("CLASS");
+    	println("Class: " + c.getName());
+    	
+    	Class superclass = c.getSuperclass();
+    	if (superclass != null) {
+    		println("SUPERCLASS -> Recursively Inspect");
+        	println("SuperClass: " + superclass.getName());
+        	inspectClass(superclass, obj, recursive, depth+1);
+    	}
+    	
+    }
+    
+    private void println(String string) {
+    	for (int i = 0; i < depth; i++) {
+    		System.out.print("\t");
+    	}
+    	System.out.println(string);
     }
 
 }
